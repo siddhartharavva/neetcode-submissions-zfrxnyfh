811f66e1c0270aecfdefab5f305986cc93cf304d
siddhartha Ravva <139237494+siddhartharavva@users.noreply.github.com>
2026-04-13 21:42:48 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l = 0 ; 
+        HashMap <Character,Integer> c = new HashMap<>();
+        int ans = 0;
+        int maxv = 0;
+        for(int r = 0 ; r < s.length();r++){
+            char val = s.charAt(r);
+            c.put(val,c.getOrDefault(val,0)+1);
+            if(c.getOrDefault(val,0)>maxv)maxv = c.get(val);
+            while((r-l+1)-maxv>k){
+                char mval = s.charAt(l);
+                c.put(mval,c.getOrDefault(mval,0)-1);
+                l++;
+
+            }
+            ans = Math.max(ans,r-l+1);
+            
+            
+        }
+        return ans;
+
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
+    public int characterReplacement(String s, int k) {
+        HashMap <Character,Integer> c = new HashMap<>();
+        int n = s.length();
+        int maxval = 0;
+        int ans = 0;
+        int l = 0;
+        for(int r = 0 ; r < n;r++){  
+            char val = s.charAt(r);
+            c.put(val,c.getOrDefault(val,0)+1);
+            if(c.getOrDefault(val,0)>maxval)maxval = c.get(val);
+            while(((r-l+1) - maxval)>k ){
+                char mval = s.charAt(l);
+                c.put(mval,c.getOrDefault(mval,0)-1);
+                l++;
+
+            }
+            ans = Math.max(ans,r-l+1);
+        }
+        return ans;
+    }
+}
+*/
